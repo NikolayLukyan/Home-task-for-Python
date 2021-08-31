9930a3d38f280670c36740c6e81c2b555c631342
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-08-31 14:00:31 +0300

    Delete Home_task_3.py

--- Home_task_3.py
+++
@@ -1,6 +0,0 @@
-user_number = input("Введите свое целое число больше 0: ")
-while user_number < '0':
-   print('Необходимо ввести целое число больше 0')
-   user_number = input("Введите свое целое число больше 0: ")
-result = int(user_number)+int(f'{user_number}{user_number}')+int(f'{user_number}{user_number}{user_number}')
-print('получаемое выражение вида: ', user_number,'+',(f'{user_number}{user_number}'),'+',(f'{user_number}{user_number}{user_number}'),'=', result )
