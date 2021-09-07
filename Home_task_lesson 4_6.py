5d6ef27406df1a74b2b031eb6423288e66363fc7
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-07 16:27:54 +0300

    Delete Home_task_lesson 4_6.py

--- Home_task_lesson 4_6.py
+++
@@ -1,5 +0,0 @@
-from itertools import cycle, count
-your_count = count(3)
-your_cycle = cycle('GFRErttyyu')
-for _ in range(6):
-    print("(your_count, your_cycle) = ({},{})".format(next(your_count), next(your_cycle)))
