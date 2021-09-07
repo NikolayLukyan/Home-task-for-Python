b8345cc55b2c52554a043f9b534aa803809ef293
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-07 16:27:46 +0300

    Delete Home_task_lesson 4_5.py

--- Home_task_lesson 4_5.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-
-
-def multi(num_1, num_2):
-    return num_1 * num_2
-
-
-new_list = [sub for sub in range(100, 1001, 2)]
-print(f' My list was\n {new_list} \n the result of multiplication is\n {reduce(multi, new_list)}\n ')
