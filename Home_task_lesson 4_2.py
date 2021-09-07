a4712d1806ec1acc9206258dc363c9bd5280b926
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-07 16:27:17 +0300

    Delete Home_task_lesson 4_2.py

--- Home_task_lesson 4_2.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-
-new_list = [randint(0, 10000) for _ in range(0, randint(0, 25))]
-next_more_previous = [new_list[num] for num in range(1, len(new_list)) if new_list[num] > new_list[num - 1]]
-print(next_more_previous)
-print(new_list)
