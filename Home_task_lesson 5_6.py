ddd28e44dbd22d9b19ca3cbb4e43430ec7cf84c0
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-10 13:20:09 +0300

    Delete Home_task_lesson 5_6.py

--- Home_task_lesson 5_6.py
+++
@@ -1,7 +0,0 @@
-dic = {}
-with open('text_6.txt', encoding='utf-8') as my_best:
-    for line in my_best:
-        name, status = line.split(':')
-        sum_name = sum(map(int,''.join([i for i in status if i == ' ' or '9' >= i >= '0' ]).split()))
-        dic[name] = sum_name
-print(f'{dic}')
