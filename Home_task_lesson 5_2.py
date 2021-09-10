4851266ce56794f39892d4b1ce5dbcc5373dfc03
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-10 13:18:25 +0300

    Delete Home_task_lesson 5_2.py

--- Home_task_lesson 5_2.py
+++
@@ -1,4 +0,0 @@
-with open('my_file_2.txt', 'r', encoding='utf-8') as my_favorite:
-    use = [f'{line}. {count.strip()} - {len(count.split())} words'
-           for line, count in enumerate(my_favorite, 1)]
-    print(*use, f'In this file you have - {len(use)} strings.', sep='\n')
