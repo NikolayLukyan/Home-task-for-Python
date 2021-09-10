babe6c61caf4251513e14c76b918eaf42a76ed41
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-10 13:18:56 +0300

    Delete Home_task_lesson 5_4.py

--- Home_task_lesson 5_4.py
+++
@@ -1,4 +0,0 @@
-our_dic = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
-with open("my_text_3.txt", "w", encoding="utf-8") as my_f:
-    with open("my_text_4.txt", encoding="utf-8") as my_new:
-        my_f.writelines([line.replace(line.split()[0], our_dic.get(line.split()[0])) for line in my_new])
