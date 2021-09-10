cff66aeee6938207f3197ab79aa03edb75601f79
NikolayLukyan <89450993+NikolayLukyan@users.noreply.github.com>
2021-09-10 13:18:37 +0300

    Delete Home_task_lesson 5_3.py

--- Home_task_lesson 5_3.py
+++
@@ -1,4 +0,0 @@
-with open('my_file_5.txt', 'r', encoding='utf-8') as my_favorite:
-    dict_of_employees = {line.split()[0]: float(line.split()[1]) for line in my_favorite}
-    print(f'The average salary in this firm is {round(sum(dict_of_employees.values()) / len(dict_of_employees), 3)}\n'
-          f'Employees who have salary less than 20.000 {[el[0] for el in dict_of_employees.items() if el[1] < 20000]}')
